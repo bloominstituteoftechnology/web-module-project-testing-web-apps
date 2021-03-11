c43c13da2c058cbee8654c276736753e19bedd73
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-11 14:05:12 -0800

    adds

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -2,3 +2,6 @@
 1. What are some possible tests for this application?
 * The component correctly renders.
 * ...
+1.fill out form and see if fields are set to initial state
+2.can the submit button be pressed with only white space in one of the fields
+3. can form be sumitted without message?
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,23 +5,63 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm />);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm />);
+    const header = screen.queryByText(/contact form/i);
+
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+    expect(header).toHaveTextContent(/contact form/i);
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    render(<ContactForm />);
+    const firstNameInput = screen.getByLabelText('First Name*')
+    userEvent.type(firstNameInput, 'asdf')
+
+    await waitFor(() => {
+        expect(firstNameInput).toThrowError
+    })
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm />)
+
+    const button = screen.getByRole('button')
+    const firstNameInput = screen.getByLabelText('First Name*')
+    const LastNameInput = screen.getByLabelText('Last Name*')
+    const emailInput = screen.getByLabelText('Email*')
+
+    userEvent.click(button)
+
+    await waitFor(() => {
+        expect(firstNameInput).toThrowError()
+        expect(LastNameInput).toThrowError()
+        expect(emailInput).toThrowError()
+    })
+
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+    // render(<ContactForm />)
+    // const Pookie = 'Pookie'
+    // const Mookie = 'Mookie'
+    // const Wookie = 'Wookie'
+    // const button = screen.getByRole('button')
+    // const firstNameInput = screen.getByLabelText('First Name*')
+    // const LastNameInput = screen.getByLabelText('Last Name*')
+    // const emailInput = screen.getByLabelText('Email*')
+
+    // userEvent.type(firstNameInput, Pookie)
+    // userEvent.type(LastNameInput, Mookie)
+    // userEvent.click(button)
+
+    // await waitFor(() => {
+    //     expect(firstNameInput).toThrowError()
+    // })
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
