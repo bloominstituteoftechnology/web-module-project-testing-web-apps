a92571027c7ab17a97ad80a2550fb0910ec3739b
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-04-09 10:49:56 -0400

    having trouble writing test will rewatch assignment video and comeback to it in a couple hours

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,130 +1,136 @@
 import React, { useState } from "react";
-import DisplayComponent from './DisplayComponent';
+import DisplayComponent from "./DisplayComponent";
 
 const formData = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  message: ""
-}
-
+	firstName: "",
+	lastName: "",
+	email: "",
+	message: "",
+};
+// wd
 const errorData = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  message: ""
-}
+	firstName: "",
+	lastName: "",
+	email: "",
+	message: "",
+};
 
 const ContactForm = () => {
-  const [displayData, setDisplayData] = useState(false);
-  const [form, setForm] = useState(formData);
-  const [errors, setErrors] = useState(errorData);
+	const [displayData, setDisplayData] = useState(false);
+	const [form, setForm] = useState(formData);
+	const [errors, setErrors] = useState(errorData);
 
-  const errorHandling = (fieldName, fieldValue) => {
-    if (fieldName === "firstName" && fieldValue.length < 5)
-      return `${fieldName} must have at least 5 characters.`;
+	const errorHandling = (fieldName, fieldValue) => {
+		if (fieldName === "firstName" && fieldValue.length < 5)
+			return `${fieldName} must have at least 5 characters.`;
 
-    const emailRegex = /(.*)@(.*)\.(.+)/g;
-    if (fieldName === "email" && !fieldValue.match(emailRegex))
-      return `${fieldName} must be a valid email address.`;
+		const emailRegex = /(.*)@(.*)\.(.+)/g;
+		if (fieldName === "email" && !fieldValue.match(emailRegex))
+			return `${fieldName} must be a valid email address.`;
 
-    if (fieldName !== "message" && fieldValue === "")
-      return `${fieldName} is a required field.`;
-    
-    return "";
-  }
+		if (fieldName !== "message" && fieldValue === "")
+			return `${fieldName} is a required field.`;
 
+		return "";
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    const submitErrors = {};
-    Object.keys(errors).forEach(field => {
-      submitErrors[field] = errorHandling(field, form[field])
-    });
-    
-    setErrors(submitErrors);
-    
-    const hasErrors = (submitErrors.firstName === "" && submitErrors.lastName === "" && submitErrors.email === "" && submitErrors.message === "");
-    setDisplayData(hasErrors);
-      
-  };
+		const submitErrors = {};
+		Object.keys(errors).forEach((field) => {
+			submitErrors[field] = errorHandling(field, form[field]);
+		});
 
-  const handleChange = (e) => {
-    const errorMessage = errorHandling(e.target.name, e.target.value);
+		setErrors(submitErrors);
 
-    if (errorMessage !== "") {
-      setDisplayData(false);
-    }
+		const hasErrors =
+			submitErrors.firstName === "" &&
+			submitErrors.lastName === "" &&
+			submitErrors.email === "" &&
+			submitErrors.message === "";
+		setDisplayData(hasErrors);
+	};
 
-    setErrors({
-      ...errors,
-      [e.target.name]: errorMessage
-    });
+	const handleChange = (e) => {
+		const errorMessage = errorHandling(e.target.name, e.target.value);
 
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value
-    });
-  }
+		if (errorMessage !== "") {
+			setDisplayData(false);
+		}
 
-  return (
-    <div className="App">
-      <h1>Contact Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="firstName">First Name*</label>
-          <input
-            onChange={handleChange}
-            name="firstName"
-            value={form.firstName}
-            id="firstName"
-            placeholder="Edd"
-          />
-          {(errors.firstName) && <p data-testid="error">Error: {errors.firstName}</p>}
-        </div>
+		setErrors({
+			...errors,
+			[e.target.name]: errorMessage,
+		});
 
-        <div>
-          <label htmlFor="lastName">Last Name*</label>
-          <input
-            onChange={handleChange}
-            id="lastName"
-            name="lastName"
-            value={form.lastName}
-            placeholder="Burke"
-          />
-          {(errors.lastName) && <p data-testid="error">Error: {errors.lastName}</p>}
-        </div>
+		setForm({
+			...form,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-        <div>
-          <label htmlFor="email">Email*</label>
-          <input 
-            onChange={handleChange}
-            id="email"
-            name="email" 
-            value={form.email}
-            placeholder="bluebill1049@hotmail.com"
-          />
-          {(errors.email) && <p data-testid="error">Error: {errors.email}</p>}
-        </div>
+	return (
+		<div className="App">
+			<h1>Contact Form</h1>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="firstName">First Name*</label>
+					<input
+						onChange={handleChange}
+						name="firstName"
+						value={form.firstName}
+						id="firstName"
+						placeholder="Edd"
+					/>
+					{errors.firstName && (
+						<p data-testid="error">Error: {errors.firstName}</p>
+					)}
+				</div>
 
-        <div>
-          <label htmlFor="message">Message</label>
-          <textarea
-            onChange={handleChange}
-            name="message"
-            id="message"
-            value={form.message}
-          />
-          {(errors.message) && <p data-testid="error">Error: {errors.message}</p>}
-        </div>
+				<div>
+					<label htmlFor="lastName">Last Name*</label>
+					<input
+						onChange={handleChange}
+						id="lastName"
+						name="lastName"
+						value={form.lastName}
+						placeholder="Burke"
+					/>
+					{errors.lastName && (
+						<p data-testid="error">Error: {errors.lastName}</p>
+					)}
+				</div>
 
-        {displayData && <DisplayComponent form={form}/>}
+				<div>
+					<label htmlFor="email">Email*</label>
+					<input
+						onChange={handleChange}
+						id="email"
+						name="email"
+						value={form.email}
+						placeholder="bluebill1049@hotmail.com"
+					/>
+					{errors.email && <p data-testid="error">Error: {errors.email}</p>}
+				</div>
 
-        <input type="submit" />
-      </form>
-    </div>
-  );
+				<div>
+					<label htmlFor="message">Message</label>
+					<textarea
+						onChange={handleChange}
+						name="message"
+						id="message"
+						value={form.message}
+					/>
+					{errors.message && <p data-testid="error">Error: {errors.message}</p>}
+				</div>
+
+				{displayData && <DisplayComponent form={form} />}
+
+				<input type="submit" />
+			</form>
+		</div>
+	);
 };
 
 export default ContactForm;
