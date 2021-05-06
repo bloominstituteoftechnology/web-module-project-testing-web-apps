8a00a6df38b8f64b0be3cee8e5a258ccb407eee2
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-05-06 17:23:38 -0400

    completed test 3 / renders an error if name field is left unpopulated

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -12,14 +12,32 @@ test("renders the contact form header", () => {
   //Arrange
   render(<ContactForm />);
   //Act
-  const contactHeader = screen.getByText("Contact Form");
+  const contactHeader = screen.getByText(/contact form/i);
   //Assert
   expect(contactHeader).toBeInTheDocument();
 });
 
-test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {});
+test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
+  render(<ContactForm />);
 
-test("renders THREE error messages if user enters no values into any fields.", async () => {});
+  const firstNameInput = screen.getByLabelText(/first name/i);
+
+  userEvent.type(firstNameInput, "kaz");
+
+  const err = await screen.findByTestId(/error/i);
+  expect(err).toBeInTheDocument();
+});
+
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  //render
+  render(<ContactForm />);
+  //query for all inputs
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const emailInput = screen.getByLabelText(/email/i);
+
+  //type into input
+});
 
 test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
 
