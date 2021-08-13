db7e4839baa2fa6117690e1f636e9479eefa02bb
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-08-12 21:49:58 -0700

    got 1st 8 working

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -11,36 +11,196 @@ test('renders without errors', ()=>{
 test('renders the contact form header', ()=> {
     // Arrange
     render(<ContactForm />);
+    // This prints the DOM as the test sees it, when done after sanity check remove it
+    // screen.debug();
     // Act
-    const headerText = screen.getByText(/Lambda Integration Testing Challenge/i);
+    const headerText = screen.getByText(/Contact Form/i);
     // Assert
-    expect(headerText);
+    // make the test fail to 'sanity check' it
+    // expect(headerText).not.toBeInTheDocument();
+    expect(headerText).toBeInTheDocument();
 });
 
+
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+    // Arrange ??? is it necessary to render the entire contact form ???
+    render(<ContactForm />)
+    // Act
+    const firstName = screen.getByLabelText(/First Name*/i);
+    userEvent.type(firstName, "firs");
+
+    const lastName = screen.getByLabelText(/Last Name*/i);
+    userEvent.type(lastName, "Ali");
     
+    const email = screen.getByLabelText(/Email*/i);
+    userEvent.type(email, "tom@hotmail.com");
+
+    const message = screen.getByLabelText(/Message/i);
+    userEvent.type(message, "message");
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    //screen.debug();
+    
+    const errorMessage = screen.getAllByTestId("error");
+    // Assert
+    // Checking to see if just one error is there
+    // sanity check 
+    // expect(errorMessage).toHaveLength(2);
+    expect(errorMessage).toHaveLength(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
+    // Arrange
+    render(<ContactForm />);
+
+    // Act
+    const firstName = screen.getByLabelText(/First Name*/i);
+    userEvent.type(firstName, "");
+
+    const lastName = screen.getByLabelText(/Last Name*/i);
+    userEvent.type(lastName, "");
     
+    const email = screen.getByLabelText(/Email*/i);
+    userEvent.type(email, "");
+
+    const message = screen.getByLabelText(/Message/i);
+    userEvent.type(message, "");
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+
+   // screen.debug();
+
+    const errorMessages = screen.getAllByTestId("error");
+    // Assert
+    expect (errorMessages).toHaveLength(3);
+
+
 });
 
-test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
-});
+ test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
+// Arrange
+render(<ContactForm />);
+
+// Act
+const firstName = screen.getByLabelText(/First Name*/i);
+userEvent.type(firstName, "Alieze");
+
+const lastName = screen.getByLabelText(/Last Name*/i);
+userEvent.type(lastName, "Ali");
+
+const email = screen.getByLabelText(/Email*/i);
+userEvent.type(email, "");
+
+const message = screen.getByLabelText(/Message/i);
+userEvent.type(message, "");
+
+const button = screen.getByRole("button");
+userEvent.click(button);
+
+const errorMessages = screen.getAllByTestId("error");
+
+// Assert
+// Sanity Check
+// expect (errorMessages).toHaveLength(2);
+expect (errorMessages).toHaveLength(1);
+
+
+ });
+
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
-});
+// Arrange
+render(<ContactForm />);
+
+// Act
+const firstName = screen.getByLabelText(/First Name*/i);
+userEvent.type(firstName, "Alieze");
+
+const lastName = screen.getByLabelText(/Last Name*/i);
+userEvent.type(lastName, "Ali");
+
+const email = screen.getByLabelText(/Email*/i);
+userEvent.type(email, "ksjsjf");
+
+const message = screen.getByLabelText(/Message/i);
+userEvent.type(message, "message");
+
+const button = screen.getByRole("button");
+userEvent.click(button);
+
+const errorMessages = screen.getAllByTestId("error");
+
+// Assert
+// Sanity Check
+// expect (errorMessages).toHaveLength(2);
+expect (errorMessages).toHaveLength(1);
+// expect (errorMessages[0]).toHaveTextContent("email must be a valid email addressss");
+expect (errorMessages[0]).toHaveTextContent("email must be a valid email address");
+
+ });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
-});
+
+// Arrange
+render(<ContactForm />);
+
+// Act
+const firstName = screen.getByLabelText(/First Name*/i);
+userEvent.type(firstName, "Alieze");
+
+const lastName = screen.getByLabelText(/Last Name*/i);
+userEvent.type(lastName, "");
+
+const email = screen.getByLabelText(/Email*/i);
+userEvent.type(email, "keanu@gmail.com");
+
+const message = screen.getByLabelText(/Message/i);
+userEvent.type(message, "message");
+
+const button = screen.getByRole("button");
+userEvent.click(button);
+
+const errorMessages = screen.getAllByTestId("error");
+// screen.debug();
+// Assert
+// Sanity Check
+// expect (errorMessages).toHaveLength(2);
+expect (errorMessages).toHaveLength(1);
+//expect (errorMessages[0]).toHaveTextContent("email must be a valid email addressss");
+expect (errorMessages[0]).toHaveTextContent("lastName is a required field");
+ });
+
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
-});
+// Arrange
+render(<ContactForm />);
 
-test('renders all fields text when all fields are submitted.', async () => {
+// Act
+const firstName = screen.getByLabelText(/First Name*/i);
+userEvent.type(firstName, "Alieze");
+
+const lastName = screen.getByLabelText(/Last Name*/i);
+userEvent.type(lastName, "Ali");
+
+const email = screen.getByLabelText(/Email*/i);
+userEvent.type(email, "keanu@gmail.com");
+
+const message = screen.getByLabelText(/Message/i);
+userEvent.type(message, "");
+
+const button = screen.getByRole("button");
+userEvent.click(button);
+// ??? Need help understanding what's going on here
+const messageDisplay = screen.queryByTestId("messageDisplay");
+screen.debug();
+// Assert
+expect(messageDisplay).toBeNull();
+
+ });
+
+// test('renders all fields text when all fields are submitted.', async () => {
     
-});
+// });
