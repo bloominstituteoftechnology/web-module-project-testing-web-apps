101098793f261363f55dd089d802af16aeb94959
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-08-14 13:42:03 -0500

    rewatching the lecture to fully understand what I am doing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,19 +5,45 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+   render(<ContactForm />)
 });
 
 test('renders the contact form header', ()=> {
-    
+	const { getByText } = render(<ContactForm />)
+	const header = getByText(/contact form/i)
+
+	expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+	render(<ContactForm />)
+	// first I need to find if the app contains the input field first name
+	const firstName = screen.getByLabelText(/First Name*/i)
+	// then type a name with 5 characters
+	userEvent.type(firstName, 'stev');
+	//check for error message
+	const error = screen.getAllByTestId("error");
+	//check if the error displays
+	expect(error)
+
+	
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+	// render(<ContactForm />)
+	
+	// const firstName = screen.getByLabelText(/First Name*/i)
+	// const lastName = screen.getByLabelText(/Last Name/i)
+	// const email = screen.getByLabelText(/email/i)
+
+	// userEvent.type(firstName, '')
+	// userEvent.type(lastName,'')
+	// userEvent.type(email, '') 
+
+	// const error = screen.getAllByTestId(/error/i)
+
+	// expect(error).toBeInTheDocument();
+
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
