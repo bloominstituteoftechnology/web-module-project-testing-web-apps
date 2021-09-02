bffd95af5816c9fc282b95e4f2201ff652f71e6f
Klove Adams <kalebsxott@gmail.com>
2021-09-02 15:25:15 -0400

    First test works

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,11 +5,11 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-  
+  render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
-    
+  
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
