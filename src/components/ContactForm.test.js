408246c0381263441701e92dcc13d7e2fbb63c5c
Klove Adams <kalebsxott@gmail.com>
2021-09-02 15:49:35 -0400

    Starting test 5

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -35,8 +35,13 @@ test('renders ONE error message if user enters less then 5 characters into first
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
   //Arrange:
+  render(<ContactForm/>);
   //Act:
+  const submitButton = screen.getByRole('button');
+  userEvent.click(submitButton);
   //Assert:
+  const errorMessagesInDOMA = screen.queryAllByTestId(/error/i);
+  expect(errorMessagesInDOMA).toHaveLength(3);
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
