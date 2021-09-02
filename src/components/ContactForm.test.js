1129c8d030c8b832f8081047c323a0f3918577cc
Klove Adams <kalebsxott@gmail.com>
2021-09-02 16:04:28 -0400

    Starting test 6

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -40,18 +40,31 @@ test('renders THREE error messages if user enters no values into any fields.', a
   const submitButton = screen.getByRole('button');
   userEvent.click(submitButton);
   //Assert:
-  const errorMessagesInDOMA = screen.queryAllByTestId(/error/i);
-  expect(errorMessagesInDOMA).toHaveLength(3);
+  const allThreeErrors = screen.queryAllByTestId(/error/i);
+  expect(allThreeErrors).toHaveLength(3);
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
   //Arrange:
+  render(<ContactForm/>);
   //Act:
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/Last Name/i);
+  const button = screen.getByRole('button');
+
+  userEvent.type(firstNameInput, "Klove");
+  userEvent.type(lastNameInput, "Farlen");
+  userEvent.click(button);
   //Assert:
+  const emailError = screen.queryAllByText(/error/i);
+  expect(emailError).toHaveLength(1);
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+  //Arrange:
+  
+  //Act:
+  //Assert:
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
