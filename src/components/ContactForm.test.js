1255fa950d4da9d0f051993c076ae1977079e8f1
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-09-30 21:35:09 -0700

    completed test 6

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -97,23 +97,54 @@ test('renders ONE error message if user enters a valid first name and last name 
 // the component renders the text *"email must be a valid email address"* if an invalid email address is typed into the email field.
 test('renders "email must be a valid email address" if an invalid email is entered', async () =>
 {
+    // Arrange: render Contact Form
+    render(<ContactForm />);
+
+    // Act: Enter invalid email
+    // 1. Give email input focus
+    const emailInput = screen.getByLabelText(/email/i);
+
+    // 2. Type invalid email
+    userEvent.type(emailInput, "fake");
+
+    // Assert: Check to see if email error message appears
+    const emailError = await screen.findByText(/email must be a valid email address/i);
+    expect(emailError).toBeInTheDocument();
 
 });
 
 // the component renders the text *"lastName is a required field"* the form is submitted without a last name.
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () =>
 {
+    // Arrange: render Contact Form
+    render(<ContactForm />);
+
+    // Act:
+
+    // Assert:
 
 });
 
 // the component renders the firstname, lastname and email text when submitted with valued fields and does **not** render a message value when one is not entered into the message field.
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () =>
 {
+    // Arrange: render Contact Form
+    render(<ContactForm />);
+
+    // Act:
+
+    // Assert:
 
 });
 
 // renders all fields when the user submits with valid text filled in for all fields.
 test('renders all fields text when all fields are submitted.', async () =>
 {
+    // Arrange: render Contact Form
+    render(<ContactForm />);
+
+    // Act:
+
+    // Assert:
 
 });
