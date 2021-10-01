3912a47d48a31b059a2a1f232d8a28447377eee8
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2021-09-30 23:55:16 -0500

    some test working

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,12 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+test("on mount, new animal header renders", () => {
+    render(<App/>);
+    const aTag = screen.queryByText(/Lambda Integration Testing Challengel/i);
+    expect(aTag).toHaveTextContent(/Lambda Integration Testing Challenge/i);
+    expect(aTag).toBeInTheDocument();
+});
