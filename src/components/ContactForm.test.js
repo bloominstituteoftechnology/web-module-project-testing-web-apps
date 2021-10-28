537ae060181f3a8e5aba58e02b0a5a8247547544
Angelique Abacajan <ma_utsey@yahoo.com>
2021-10-28 13:59:03 -0400

    renders without errors

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm/>)
 });
 
 test('renders the contact form header', ()=> {
