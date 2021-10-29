d0c7e66eb796d589c2189b7e0cd644e9857f3d46
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-10-28 22:33:34 -0500

    5 test completed and passing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,25 +5,43 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm/>);
+    const headerText = screen.getByText(/Contact Form*/i)
 });
 
-test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {render
+    (<ContactForm/>);
+    const oneError = screen.getByLabelText(/First name*/i);
+    userEvent.type(oneError, 'abcd')
+    const oneErrorRen = await screen.findByTestId(/error*/i)
+    expect(oneErrorRen).toBeInTheDocument();
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm/>);
+    const button =await screen.getByRole('button');
+    userEvent.click(button);
+    const errorMessage =await screen.findAllByTestId('error');
+    expect(errorMessage).toHaveLength(3);
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+    render(<ContactForm/>);
+    const firstName = screen.getByLabelText(/First name*/i);
+    userEvent.type(firstName, 'Cheyenne')
+    const lastName = screen.getByLabelText(/Last name*/i);
+    userEvent.type(lastName, 'Bowman')
+    const missingEmail =await screen.getByLabelText(/Email*/i);
+    userEvent.type(missingEmail, '')
+    const button =await screen.getByRole('button');
+    userEvent.click(button);
+    const errorMessage =await screen.findAllByTestId('error');
+    expect(errorMessage).toHaveLength(1);
 });
-
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
     
 });
