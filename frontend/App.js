bd643a0c9ef59de9909967bacf306ddfe739ad99
Ladrillo <gabriel@cabrejas.net>
2022-03-02 16:17:28 +0100

    initial

---
+++ frontend/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ContactForm from './components/ContactForm';
+
+const App = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Integration Testing Challenge</a>
+      </nav>
+      <div className="App">
+        <ContactForm />
+      </div>
+    </div>
+  )
+}
+
+export default App;
